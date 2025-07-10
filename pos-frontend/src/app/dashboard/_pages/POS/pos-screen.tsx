0c4b4917b0fd60e68e9a75fc0bdc3f803862bc10
Jan Ryan Ballasio <98378424+JanBallasio@users.noreply.github.com>
2025-07-10 17:51:06 +0800

    feat(pos): implement payment calculator

--- pos-frontend/src/app/dashboard/_pages/POS/pos-screen.tsx
+++ pos-frontend/src/app/dashboard/_pages/POS/pos-screen.tsx
@@ -2,22 +2,23 @@ import POSHeader from "./components/pos-header";
 import POSLeftCol from "./components/pos-leftcol";
 import POSRightColUpper from "./components/pos-rightcol-upper";
 import POSRightColLower from "./components/pos-rightcol-lower";
+
+import { CartProvider } from "@/contexts/cart-context";
+
 export default function MainDashboard() {
   return (
-    <div className="flex flex-col w-full h-[880px] bg-neutral-100 py-4 px-4">
-      <div className="flex gap-2 h-full">
-        <div className="flex-[70%]">
-          <POSLeftCol />
-        </div>  
-        <div className="flex flex-col flex-[30%] gap-2">
-          <div className="flex-[50%]">
-            <POSRightColUpper />
+    <CartProvider>
+      <div className="flex flex-col w-full h-[880px] bg-neutral-100 py-4 px-4"
+      >
+        <div className="flex gap-2 h-full">
+          <div className="flex-[70%]">
+            <POSLeftCol />
           </div>
-          <div className="flex-[50%]">
-            <POSRightColLower />
+          <div className="flex flex-col flex-[30%] gap-2">
+            <POSRightColUpper />
           </div>
         </div>
       </div>
-    </div>
+    </CartProvider>
   );
 }
--- pos-frontend/src/app/dashboard/_pages/Products/page.tsx
+++ pos-frontend/src/app/dashboard/_pages/Products/page.tsx
@@ -49,7 +49,7 @@ export default function ProductsPage() {
     fetchUsers();
   }, []);
 
-  return (
+  return (  
     <div className="flex flex-col gap-4 p-4">
       <h1 className="text-2xl font-bold">Products</h1>
       
