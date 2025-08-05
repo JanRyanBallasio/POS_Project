3e3af5d1e9cbf70386d8cd0f6e2923eed3633ca2
Jan Ryan Ballasio <jryan6278@gmail.com>
2025-08-05 19:31:06 +0800

    deploy

--- pos-backend/src/middleware/cors.js
+++ pos-backend/src/middleware/cors.js
@@ -1,3 +1,18 @@
 const cors = require('cors');
 
+const allowedOrigins = [
+    "http://localhost:5000",
+    "http://13.211.162.106:5000"
+];
+
+const options = {
+    origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    }
+};
+
 module.exports = cors();
