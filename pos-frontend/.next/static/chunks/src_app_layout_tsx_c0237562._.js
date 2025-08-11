ff4fb922a6afbbe139d99459b0b05dfb4455664a
Jan Ryan Ballasio <jryan6278@gmail.com>
2025-08-11 20:24:12 +0800

    feat(dashboard):make the graph responsive

--- pos-frontend/.next/static/chunks/src_app_layout_tsx_c0237562._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__cd6500b7._.css",
-  "static/chunks/_b13d0b3a._.js"
-],
-    source: "dynamic"
-});
