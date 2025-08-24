9437515cf529971fd358e8c472f2a93e1635fb3e
Jan Ryan Ballasio <jryan6278@gmail.com>
2025-08-25 07:28:03 +0800

    feat(products):added vategory modal and functionality

--- pos-frontend/.next/static/chunks/src_app_dashboard_layout_tsx_b2eb9dbe._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_6f926ceb._.js",
-  "static/chunks/node_modules_21cd66df._.js"
-],
-    source: "dynamic"
-});
