6c49dd1bc2ce1da4c820ae15ed6dac6be08547f6
Jan Ryan Ballasio <jryan6278@gmail.com>
2025-09-04 19:16:00 +0800

    Fix refresh token 401 errors on login page

--- pos-frontend/.next/server/server-reference-manifest.js
+++ pos-frontend/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"990NMPXctuN/N8zYUc4Q1vQYRnoCXO01ZB8zHVA9zmg=\"\n}"
