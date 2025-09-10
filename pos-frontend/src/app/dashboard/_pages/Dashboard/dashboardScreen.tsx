9ab409cd477f6276d8c59b2b2375e2e67840ad21
Jan Ryan Ballasio <jryan6278@gmail.com>
2025-09-10 18:41:02 +0800

    feat(product-modal): improve responsiveness and add icon-based filter buttons

--- pos-frontend/src/app/dashboard/_pages/Dashboard/dashboardScreen.tsx
+++ pos-frontend/src/app/dashboard/_pages/Dashboard/dashboardScreen.tsx
@@ -10,8 +10,8 @@ export default function DashboardScreen() {
                 <p>Welcome Back! Here's your business overview!</p>
             </div>
             <div className="flex-[20%]"><Stats /></div>
-            <div className="flex-[78%] flex     flex-col">
-                <div className="flex-[50%]"><ProductStats /></div>
+            <div className="flex-[78%] flex gap-3 flex-col">
+                <div className="flex-[50%] mb-2"><ProductStats /></div>
                 <div className="flex-[50%]"><SalesStats /></div>
             </div>
         </div>
