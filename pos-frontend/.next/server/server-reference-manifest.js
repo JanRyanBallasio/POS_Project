71e8b4f1c57fbfcdf3ab1e9a73fad26ac3fc5000
Jan Ryan Ballasio <jryan6278@gmail.com>
2025-09-11 07:34:24 +0800

    fix: ensure sales and sale items use Manila 12AM–11:59PM range mapped to UTC

--- pos-frontend/.next/server/server-reference-manifest.js
+++ pos-frontend/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"FJhTXwXMWkb+omzyklQYLPaFw8/8mE7yM/aqe0eA1RA=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
