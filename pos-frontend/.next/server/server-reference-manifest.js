7ea990a531b057b329f22dc2fcaff730ad3c6dc2
Jan Ryan Ballasio <jryan6278@gmail.com>
2025-10-01 11:36:16 +0800

    feat(receipt): fix UI layout consistency and add smart auto-cut functionality

--- pos-frontend/.next/server/server-reference-manifest.js
+++ pos-frontend/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"9TbBp7mroeCuo82xTIEhCtSeMAexGct0IcK87U+gMco=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
