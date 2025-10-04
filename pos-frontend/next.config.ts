3bdbbfbefab43fcd02ebf43d00d99e7182f0f854
Jan Ryan Ballasio <jryan6278@gmail.com>
2025-10-04 08:24:30 +0800

    build

--- pos-frontend/next.config.ts
+++ pos-frontend/next.config.ts
@@ -1,15 +1,43 @@
+// //next.config.ts
+
+// import type { NextConfig } from "next";
+
+// const isProd = process.env.NODE_ENV === "production";
+
+// const nextConfig: NextConfig = {
+//   // output: "export",
+//   assetPrefix: isProd ? "" : undefined,
+//   reactStrictMode: true,
+//   productionBrowserSourceMaps: !isProd,
+//   images: {
+//     unoptimized: true,
+//   },
+// };
+
+// export default nextConfig;
+
+
+
+
+// next.config.ts
 import type { NextConfig } from "next";
 
 const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
-  output: "export",
+  // output: "export", // ⚠️ keep disabled for SSR
   assetPrefix: isProd ? "" : undefined,
   reactStrictMode: true,
   productionBrowserSourceMaps: !isProd,
   images: {
     unoptimized: true,
   },
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    NEXT_PUBLIC_BACKEND_ORIGIN: process.env.NEXT_PUBLIC_BACKEND_ORIGIN,
+  },
 };
 
 export default nextConfig;
+
+
